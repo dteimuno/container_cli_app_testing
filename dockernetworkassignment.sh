34a3bb4bf689b4233d7c717e31224e833e160de9
Dennis Tei-Muno <munodennis@gmail.com>
2025-01-01 04:26:09 -0600

    Update dockernetworkassignment.sh

--- dockernetworkassignment.sh
+++ dockernetworkassignment.sh
@@ -1,1 +1,10 @@
+#Create container network
+docker network create dtmnet
+#creat two new containers
+docker container run -d --name container1 --network-alias search elasticsearch:2
+docker container run -d --name container2 --network-alias search elasticsearch:2
+
+#DNS robin caching work
+docker container run --rm --network dtmnet alpine nslookup search
+docker container run --rm --network dtmnet centos curl -s search:9200
 
